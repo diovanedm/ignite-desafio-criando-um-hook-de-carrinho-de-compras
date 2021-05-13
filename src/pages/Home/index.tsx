06e3a936582e81a72ffc9478df9b00c937dd2e3e
Diovane Maia <diovane.dm@gmail.com>
2021-05-13 09:30:56 -0300

    Load products

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,7 +22,7 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
   // const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
@@ -31,7 +31,9 @@ const Home = (): JSX.Element => {
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      const response = await api.get('stock') 
+        .then(response => setProducts(response.data))
+        .then(data => console.log(products))
     }
 
     loadProducts();
