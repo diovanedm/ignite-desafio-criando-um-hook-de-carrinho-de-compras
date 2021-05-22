81d86957466e4078affae229e98c20d98e3b3878
diovanedm <diovane.dm@gmail.com>
2021-05-22 17:20:11 -0300

    Finally Exercise

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,67 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
-  const addProduct = async (productId: number) => {
+  const addProduct = async (productId: number) => { 
     try {
       // TODO
-    } catch {
-      // TODO
+
+      const productExist = cart.find(product => product.id === productId);
+      const stock: Stock = await api.get(`stock?id=${productId}`)
+      .then(response => response.data[0]);
+      
+      if(productExist) {
+        
+        if(productExist.amount < stock.amount) {
+          const newAmount = cart.map(product => product.id == productId ? {
+            ...product, // Copia o valor do product
+            amount: product.amount + 1 // Altera o valor do atributo
+          } : product);
+          
+          setCart(newAmount)
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(newAmount))
+        } else {
+          toast.error('Produto não tem mais no estoque');
+        }
+        
+      } else {
+        const product: Product = await api.get(`products?id=${productId}`)
+        .then(response => response.data[0])
+
+        const productAcrt = [...cart, {
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          image: product.image,
+          amount: 1
+        }]
+
+        setCart(productAcrt)
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(productAcrt))
+      }  
+
+      } catch (error) {
+        toast.error('Erro');    
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const removeProductCart = cart.filter(products => products.id != productId)
+      
+      setCart(removeProductCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(removeProductCart))
     } catch {
       // TODO
+      toast.error('Erro');    
     }
   };
 
@@ -53,9 +92,25 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+
+      const productStock: Stock = await api.get(`stock?id=${productId}`)
+        .then(response => response.data[0])            
+ 
+      if(amount <= productStock.amount) {
+        const newProductAmount = cart.map(product => product.id === productId ? {
+          ...product, 
+          amount
+        }: product)
+
+        setCart(newProductAmount)  
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newProductAmount))
+      } else {
+        toast.error('Produto não tem mais no estoque');
+      }
+      
     } catch {
       // TODO
+      toast.error('Erro');    
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,9 +25,10 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+    return sumAmount
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -39,8 +40,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    const product = products.filter(product => id === product.id)
-    addProduct(product[0].id)
+    addProduct(id)
   }
 
   return (
@@ -64,7 +64,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
